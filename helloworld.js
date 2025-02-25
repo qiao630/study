24d7de2cdd75374be7c37ca8b252a4bdab994aa3
qiao630 <98213598+qiao630@users.noreply.github.com>
2025-02-25 02:47:01 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,18 @@
-const hp = require("https");
-console.log("Hello, Qiao's world!");
+const http=require("http");
+const https=require("https");
+const getOption={
+	hostname: 'www.baidu.com',
+	port: 443,
+	path: "/",
+	method: 'GET',
+}
+
+let req=https.request(getOption, res=>{
+	data="";
+	res.on('data', chunk=>{data+=chunk;});
+	res.on('end', ()=>{
+		console.log(data);
+	})
+})
+req.on('error', err=>{console.log("ERROR when get web: "+ err.message)});
+req.end();
